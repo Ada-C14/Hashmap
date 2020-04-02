ed472f1996adffb9677751ca8e0cd8fe411b8dbc
kate myer <kate.d.mangubat@gmail.com>
2020-04-02 11:16:30 -0700

    going to redo this to pass tests

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,35 +1,5 @@
 
 def palindrome_permutation?(string)
 
-  hash = {}
-  string_array = string.split("")
-  string_array.each do |letter|
-    hash[letter] = true
-  end
-
-  string_array.each do |letter|
-    hash[letter] = !hash[letter]
-  end
-  # even
-  # if the length is even we make each letter is repeat once
-  if string_array.length % 2 == 0
-    hash.values.each do |value|
-      if value == false
-        return false
-      end
-    end
-    return true
-  end
-
-  # odd
-  # if the length is odd we each letter will repeat once the the expect of one letter only once
-  if string_array.length % 2 == 1
-    odd_numbers = 0
-    hash.values.each do |value|
-      if value == false
-        odd_numbers += 1
-      end
-    end
-    return odd_numbers == 1
-  end
+ 
 end # method
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,24 +1,4 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
-  return false if string1.length != string2.length
-  hash1 = count_chars(string1)
-  hash2 = count_chars(string2)
-  if hash1 == hash2
-    return true
-  end
-  return false
-end
-
-def count_chars(string)
-  hash = {}
-  string_array = string.split("")
-  string_array.each do |letter|
-    if !hash[letter]
-      hash[letter] = 1
-    else
-      hash[letter] += 1
-    end
-  end
-  return hash
+  raise NotImplementedError, "permutations? not implemented" 
 end
