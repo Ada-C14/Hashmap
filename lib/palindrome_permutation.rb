4d7ca5e1c993c26a2d84241b3c554d6807e68fe0
Chris M <chris@adadevelopersacademy.org>
2020-10-27 16:24:23 -0700

    Update palindrome_permutation.rb

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,20 @@
-
 def palindrome_permutation?(string)
+  letter_hash = {}
 
- 
-end # method
+  string.split("").each do |letter|
+    if letter_hash[letter] == nil
+      letter_hash[letter] = 1
+    else
+      letter_hash[letter] += 1
+    end
+  end
+
+  midpoint = 0
+  letter_hash.each_value do |times|
+    if times % 2 != 0
+      midpoint += 0
+    end
+  end
+
+  return midpoint <= 1
+end
