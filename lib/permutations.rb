6458b7b2e61b6f26cda0170ed4d31840bf41a9f8
Genevieve Neely <genevieve.hood@gmail.com>
2020-10-29 14:03:53 -0700

    permutations? method

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,26 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  return true if string1.length == 0 && string2.length == 0
+
+  return false if string1.length != string2.length
+
+  hash = {}
+  string1.each_char do |char|
+    if hash.has_key?(char)
+      hash[char] += 1
+    else
+      hash[char] = 1
+    end
+  end
+
+  string2.each_char do |char|
+    if hash[char].nil?
+      return false
+    elsif hash[char] == 1
+      hash.delete(char)
+    else
+      hash[char] -= 1
+    end
+  end
+  return true
 end
