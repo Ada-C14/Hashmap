8175361cae2580b7a1ec458739ac65a431cfc3b9
Anna Kim <anna.kim93@gmail.com>
2020-11-01 21:23:14 -0500

    created an methods to return true if str1 is a permutation of str2; passed tests

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,33 @@
+# Time complexity: O(n)
+# >> O(n) iteration through string chars in letter_count()
+# >> O(n) iteration through string chars in string2
+# >> O(n) .values (returns an array of the values)
+# >> O(n) for .any?
+
+# Space complexity: O(n)
+# >> creation of hash with char counts
+# >> creation of array when checking hash.values
+
+def letter_count(str, hash)
+  str.each_char do |char|
+    hash[char] ? hash[char] += 1 : hash[char] = 1
+  end
+end
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  # raise NotImplementedError, "permutations? not implemented"
+  hash = {}
+
+  if string1.length == string2.length
+    letter_count(string1, hash)
+
+    string2.each_char do |char|
+      hash[char] -= 1 if hash[char]
+    end
+
+    return false if hash.values.any? { |value| value > 0 }
+    return true
+  end
+
+  return false
 end
