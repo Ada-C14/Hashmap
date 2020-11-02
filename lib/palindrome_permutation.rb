86d913139766372e467079ce868768546fdab26e
Anna Kim <anna.kim93@gmail.com>
2020-11-01 21:41:56 -0500

    wrote a solution for palindrome_permutations; passed tests

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,19 @@
+# Time complexity: O(n)
+# >> O(n) for iterating through each character in input string
+
+# Space complexity: O(n)
+# >> O(n) for the creation of the char_counts hash
+# >> O(n) for #select
 
 def palindrome_permutation?(string)
+  return true if string.empty?
 
- 
-end # method
+  char_counts = {}
+
+  string.each_char do |char|
+    char_counts[char] ? char_counts[char] += 1 : char_counts[char] = 1
+  end
+
+  return false if char_counts.select { |key, val| val == 1 }.size > 1
+  return true
+end
