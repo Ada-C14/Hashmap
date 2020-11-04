ca8299d1f808f62db84e56ba92ba1fd51b8869a9
Ada <ada@Adas-MacBook-Pro.local>
2020-11-04 15:35:54 -0500

    array_intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,18 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  return [] if list1 == nil || list2 == nil || list1.length == 0 || list2.length == 0
+
+  hash = {}
+  list1.each do |element|
+    hash[element] = true
+  end
+
+  result = []
+  list2.each do |element|
+    if hash[element].nil? == false
+      result << element
+    end
+  end
+
+  return result
 
 end
