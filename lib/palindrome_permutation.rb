0638390ce68b70bd5ac85e2e9478963fa14e2153
Ada <ada@Adas-MacBook-Pro.local>
2020-11-04 17:26:39 -0500

    Took out &

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,21 @@
 
 def palindrome_permutation?(string)
 
- 
-end # method
+  if string == nil || string.length == 0
+    return true
+  end
+
+
+  string_array = string.chars
+  hash = {}
+  string_array.each_with_index do |element, index|
+    hash[index] = element
+  end
+
+  string_array_reversed = string.reverse.chars
+  string_array_reversed.each_with_index do |element, index|
+    return false if hash[index] != element
+  end
+
+  return true
+end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,27 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+
+  # if (string1 == nil && string2 == nil) || (string1.length == 0 && string2.length == 0)
+  #   return true
+  # elsif string1 == nil || string2 == nil || string1.length == 0 || string2.length == 0
+  #   return false
+  # elsif string1.length != string2.length
+  #   return false
+  # end
+
+  string1_array = string1.chars
+  string2_array = string2.chars
+
+  hash = {}
+  string1_array.each do |element|
+    hash[element] = string1_array.count(element)
+  end
+
+  string2_array.each do |element|
+    if hash[element] != string2_array.count(element)
+      return false
+    end
+  end
+
+  return true
 end
