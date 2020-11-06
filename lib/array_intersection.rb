562056685968b083959acc6ac77961274b3e845f
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-05 22:45:44 -0800

    add solutions. all tests passing

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,16 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  result = []
+  array_to_hash = Hash.new(0)
 
+  list1.each do |num|
+    array_to_hash[num] += 1
+  end
+
+  list2.each do |num|
+    if array_to_hash.key?(num)
+      result << num
+    end
+  end
+
+  return result
 end
--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,13 @@
 
 def palindrome_permutation?(string)
+  letters = string.split("")
+  count = Hash.new(0)
+  odd = 0
 
- 
-end # method
+  letters.each do |letter|
+    count[letter] += 1
+    odd += (count[letter] % 2 == 1 ? 1 : -1)
+  end
+
+  return odd <= 1
+end
