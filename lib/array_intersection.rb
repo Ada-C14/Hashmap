3eefcbe05d10d76e6791526e47ed663b4440660c
Kareha Agesa <kmaagesa@gmail.com>
2020-11-07 12:41:44 -0800

    Array intersection tests passing

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,13 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  results = []
+  # what should the values of this hash table be?
+  hash_table = Hash[list1.zip Array.new(list1.length, 0)]   # Time?
+
+  list2.each do |num|                             # Time: O(m)
+    if hash_table[num]                          # Time: O(1)
+      results.push(num)                         # Space: O(n)?
+    end
+  end
+  return results
 
 end
