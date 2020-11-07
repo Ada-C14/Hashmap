172c848f80ac3bc02e0601ee68fcc76d158123e3
Li Dai <li.lea.dai@gmail.com>
2020-11-06 18:21:54 -0800

    hash exercise

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,17 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+
+  hash = Hash.new(0)
+  string1.each_char do |char1|
+    hash[char1] += 1
+  end
+
+  string2.each_char do |char2|
+     hash[char2] -= 1
+  end
+
+  return hash.values.all?(0)
 end
+
+#time complexity os O(n) where n is the length of string
+#space complexity is O(1), only 26 alphabets
