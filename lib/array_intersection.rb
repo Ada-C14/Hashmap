ae800e060bda9012ba138bde47bcd324ddec27a6
Kareha Agesa <kmaagesa@gmail.com>
2020-11-07 15:00:15 -0800

    wow permutations works

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -4,12 +4,12 @@ def intersection(list1, list2)
   results = []
   hash_table = { }
   list1.each do |num|                           # Time: O(n)
-    hash_table[num] = true                      # is there a complexity associated with this?
-  end
+    hash_table[num] = true                      # lookups - Time: O(1)
+  end                                           # Space - at the end of the loop, space is O(n) for this hash
 
   list2.each do |num|                           # Time: O(m)
-    if hash_table[num]                          # Time: O(1)
-      results.push(num)                         # Space: O(n)
+    if hash_table[num]                          # lookup - Time: O(1)
+      results.push(num)                         # Space: O(n) - at end of loop
     end
   end
   return results
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,21 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+
+  hash_table = Hash.new(0)
+  string1.each_char do |char|
+    if hash_table[char]
+      hash_table[char] += 1
+    end
+  end
+
+  string2.each_char do |char|
+    if hash_table[char] > 0
+      hash_table[char] -= 1
+    end
+  end
+
+  hash_table.each_key do |key|
+    return false if hash_table[key] != 0
+  end
+  return true
 end
