8420cd79ef3eb1a5d517dba81e9102a66f4cae38
marks214 <aimee.oz.x@gmail.com>
2020-11-07 13:46:33 -0800

    completed 3 hash map exercises

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,29 @@
-
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+
+  return false unless string1.length == string2.length
+
+  hash = {}
+
+  string1.each_char do |char|
+    if !hash[char]
+      hash[char] = 1
+    else
+      hash[char] += 1
+    end
+  end
+
+  string2.each_char do |char|
+    if hash[char]
+      hash[char] -= 1
+    else
+      # catch characters that do not belong - wrote test
+      return false
+    end
+  end
+
+  hash.each do |char, count|
+    return false if count.negative?
+  end
+
+  return true
 end
