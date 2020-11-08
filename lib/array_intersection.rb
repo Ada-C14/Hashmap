ab949abea0606738e137aabe11223ee11a8c79e6
gessica <gessicamohr18@gmail.com>
2020-11-07 17:39:09 -0800

    array intersection method complete and passed test

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,15 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  result = []
+  hash = {}
+
+  list1.each do |num|
+    hash[num] = true
+  end
+
+  list2.each do |num|
+    result << num if hash[num]
+  end
+
+  return result
 
 end
