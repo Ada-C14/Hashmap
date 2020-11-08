36f290b80399af57cf49202d5189fffa68b1bfbc
Lina Do <l.do5147@gmail.com>
2020-11-08 10:40:43 -0800

    solved permutations? method

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,20 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  return false if string1.length != string2.length
+
+  hash = Hash.new(0)
+
+  string1.each_char do |char|
+    hash[char] += 1
+  end
+
+  string2.each_char do |char|
+    if hash[char] != 0
+      hash[char] -= 1 #prevents duplicates
+    else
+      return false
+    end
+  end
+
+  return true
 end
