5170037520f233edc723027e29233df34f79d745
Lina Do <l.do5147@gmail.com>
2020-11-08 11:27:32 -0800

    solved palindrome permutation method

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,28 @@
 
 def palindrome_permutation?(string)
 
- 
+  hash = Hash.new(true)
+
+  string.each_char do |char|
+    if hash[char] == true
+      hash[char] = false
+    else
+      hash[char] = true
+    end
+
+  end
+
+  false_count = 0
+
+  hash.each do |key, value|
+    if value == false
+      false_count += 1
+    end
+  end
+
+  if false_count > 1
+    return false
+  else
+    return true
+  end
 end # method
