4fb7923033fc736a1bcb77c0c97382d120013064
Sophie Messing <sophie.e.messing@gmail.com>
2020-11-08 11:34:19 -0800

    methods written, tests passing

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,21 @@
-def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+# time complexity: O(n + m)
 
+def intersection(list1, list2)
+
+  # store list1 elements in a hash with values = true
+  hash = {}
+  list1.each do |key|
+    hash[key] = true
+  end
+
+  # check list2 elements against list1 values (true or nil)
+  list = []
+  list2.each do |element|
+    if hash[element] == true
+      list << element
+    end
+  end
+
+  return list
 end
+
