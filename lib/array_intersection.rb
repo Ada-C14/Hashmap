14cd0de7679aa15ee93fd51e70fd11305efb5a3b
Emily Nagai <emnagai89@gmail.com>
2020-11-07 23:45:51 -0800

    array intersection passing.

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,16 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  hash_table = {}
+  result = []
 
+  list1.each do |num|
+    hash_table[num] = true
+    # I need help visualizing this. How is this adding to the hash table?
+  end
+
+  list2.each do |num|
+    if hash_table[num]
+      result << num
+    end
+  end
+  return result
 end
