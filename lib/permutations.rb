56440124a5f8ac70ac827b5501c47304c3977e74
Kareha Agesa <kmaagesa@gmail.com>
2020-11-07 16:23:05 -0800

    Adding my guesses about time/space complexity

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,22 +1,23 @@
+# Time: O(n+m)
+# Space: O(n)
 def permutations?(string1, string2)
-
   hash_table = Hash.new(0)
-  string1.each_char do |char|
+  string1.each_char do |char|                      # Time: O(n)
     if hash_table[char]
       hash_table[char] += 1
-    end
+    end                                             # Space (hash table end of loop): O(n)
   end
 
-  string2.each_char do |char|
-    if hash_table[char] > 0
+  string2.each_char do |char|                       # Time: O(m)
+    if hash_table[char] > 0                         # Time: hash lookup O(1)
       hash_table[char] -= 1
     else
       return false
-    end
-  end
+    end                                               
+  end                                                 # I believe the space complexity for the hash is O(1) after this loops completes
 
-  hash_table.each_key do |key|
-    return false if hash_table[key] != 0
+  hash_table.each_key do |key|                        # Time: O(n)
+    return false if hash_table[key] != 0              # Time: hash lookup O(1)
   end
   return true
 end
