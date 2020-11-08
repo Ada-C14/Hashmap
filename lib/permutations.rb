f8121ab549a0b60427454776a069d67b83994b4c
Lina Do <l.do5147@gmail.com>
2020-11-08 10:45:28 -0800

    added comments to permutations method

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -2,15 +2,19 @@
 def permutations?(string1, string2)
   return false if string1.length != string2.length
 
+  # initialize each value at 0
   hash = Hash.new(0)
 
+  # char is the key
+  # increase count by one each time char shows up in the string
   string1.each_char do |char|
     hash[char] += 1
   end
 
+  # checks if the char is found in the hash and the count is not zero
   string2.each_char do |char|
     if hash[char] != 0
-      hash[char] -= 1 #prevents duplicates
+      hash[char] -= 1 #prevents the character from being used again
     else
       return false
     end
