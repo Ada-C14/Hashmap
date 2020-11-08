a013bc827712a14d055ba71d81d9b1b51516894b
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-07 17:21:42 -0800

    create methods and pass the tests

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,22 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  return [] if list1.count == 0 || list2.count == 0
+  
+  longer_hash = {}
+  if list1.count > list2.count
+    longer_hash = Hash[list1.collect { |item| [item, true] }]
+  else
+    longer_hash = Hash[list2.collect { |item| [item, true] }]
+  end
 
+  intersection = []
+  if list1.count > list2.count 
+    list2.each do |num|
+      intersection.push(num) if longer_hash.key?(num)
+    end
+  else 
+    list1.each do |num|
+      intersection.push(num) if longer_hash.key?(num)
+    end
+  end
+  return intersection
 end
--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,25 @@
-
 def palindrome_permutation?(string)
+  return true if string.length == 0
 
- 
-end # method
+  hash = {}
+  string.each_char do |char|
+    if hash.key?(char)
+      hash[char] += 1
+    else
+      hash[char] = 1  
+    end  
+  end
+
+  odd_check = 0
+  if string.length.even?
+    hash.each do |key, value|
+      return false if value.odd? 
+    end
+  else
+    hash.each do |key, value|
+      odd_check += 1 if value.odd?
+      return false if odd_check > 1
+    end
+  end
+  return true
+end 
