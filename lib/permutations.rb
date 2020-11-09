16f2917802eba38f24e63dc701d7d146b2848c56
Jasmine <jasylop@gmail.com>
2020-11-08 16:42:46 -0800

    permutations? method

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,16 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  chars1 = string1.split("")
+  chars2 = string2.split("")
+
+  chars = Hash.new(0)
+  chars1.each do |char|
+    chars[char] += 1
+  end
+
+  chars2.each do |char|
+    chars[char] -= 1
+  end
+
+  return !chars.any? {|k, v| v != 0 }
 end
