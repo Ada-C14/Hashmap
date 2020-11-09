33522da054d280a7c616da395aed161a2371982f
beauttie <bak02013@pomona.edu>
2020-11-08 23:12:29 -0800

    Implemented methods to pass all tests

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,6 @@
-
 def palindrome_permutation?(string)
+  hash = Hash.new(0)
+  string.each_char { |char| hash[char] += 1 }
 
- 
-end # method
+  return (hash.values.count { |freq| freq % 2 != 0 }) <= 1
+end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,13 @@
-
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  hash = Hash.new(0)
+  string1.each_char { |char| hash[char] += 1 }
+
+  string2.each_char do |char|
+    return false if hash[char].nil?
+
+    hash[char] -= 1
+    hash[char] = nil if hash[char] == 0
+  end
+
+  return hash.values.all?(nil)
 end
