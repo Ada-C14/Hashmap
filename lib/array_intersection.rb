f568de6f600a5eede5b22cc10897c9289cbb6023
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-08 21:00:44 -0800

    wrote array_intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,21 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  shorter_list = (list1.length <= list2.length) ? list1 : list2
+  longer_list = (list1.length > list2.length) ? list1 : list2
 
+  hash_table = generate_hash_table(longer_list)
+
+  shorter_list.each do |element|
+    if hash_table.include? element
+      hash_table[element] += 1
+    end
+  end
+
+  return hash_table.keep_if { |key, value| value > 1 }.keys
 end
+
+def generate_hash_table(list)
+  hash_table = {}
+  list.each { |element| hash_table[element] = 1 }
+  return hash_table
+end
+
