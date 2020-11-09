da1f3ea0a295a1e4ed8b95d90e853fa56799facb
roshni-patel <rpatel01@wesleyan.edu>
2020-11-08 20:02:05 -0600

    Completed array intersection method

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,17 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  return [] if list1.empty? || list2.empty?
 
+  num_counts = {}
+  intersection = []
+
+  list1.each do |num|
+    num_counts[num] = true
+  end
+
+  list2.each do |num|
+    if num_counts[num]
+      intersection << num
+    end
+  end
+  return intersection
 end
