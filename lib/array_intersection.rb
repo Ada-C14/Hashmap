a879644a2e98e56ee5a1598d1bec27f023ee7172
Yingjing Lin <yjlin789@gmail.com>
2020-11-08 20:56:19 -0800

    pass all tests

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,17 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  hash = {}
+  list1.each do |num|
+    hash[num] ? (hash[num] += 1) : hash[num] = 1
+  end
 
+    result = []
+    list2.each do |num2|
+      if hash[num2]
+        result << num2
+        hash[num2] -= 1
+        hash[num2] = nil if hash[num2] == 0
+      end
+    end
+
+  return result
 end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,17 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  return false if string1.length != string2.length
+
+  hash = {}
+  string1.split("").each do |letter|
+    hash[letter] ? hash[letter] += 1 : hash[letter] = 1
+  end
+
+  string2.split("").each do |letter|
+    return false unless hash[letter]
+    hash[letter] -= 1
+    hash[letter] = nil if hash[letter] == 0
+  end
+
+  return true
 end
