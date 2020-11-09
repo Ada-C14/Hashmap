26f27515d2c7964694b078e02e0d18dea63dcb37
Emily Nagai <emnagai89@gmail.com>
2020-11-09 15:20:00 -0800

    permutations and passing

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,19 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  return false if string1.length != string2.length
+  hash = {}
+
+  string1.each_char do |letter|
+    hash[letter] ? hash[letter] += 1 : hash[letter] = 1
+  end
+
+  string2.each_char do |letter|
+    hash[letter] ? hash[letter] -= 1 : (return false)
+  end
+  
+  hash.each_value do |value|
+    return false if value != 0
+  end
+
+  return true
 end
