ccff4e8a99f128ca2a3571786724ff020a112ee1
Jasmine <jasylop@gmail.com>
2020-11-08 17:10:25 -0800

    refactored permutations method

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,5 +1,7 @@
 
 def permutations?(string1, string2)
+  return false if string1.length != string2.length
+
   chars1 = string1.split("")
   chars2 = string2.split("")
 
@@ -9,8 +11,11 @@ def permutations?(string1, string2)
   end
 
   chars2.each do |char|
-    chars[char] -= 1
+    if !chars.has_key?(char) || chars[char] == 0
+      return false
+    else
+      chars[char] -= 1
+    end
   end
-
-  return !chars.any? {|k, v| v != 0 }
+  return true
 end
