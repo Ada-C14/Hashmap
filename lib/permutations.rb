7f2c12f8a2332d5b191a7c850bd3986319bfe1ba
r-spiel <richelle.mehlhaff@gmail.com>
2020-11-08 17:51:22 -0800

    methods done

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,22 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  hash = Hash.new(0)  #set default value
+  #even nonexistant key returns 0
+
+  string1.each_char do |char|
+    hash[char] += 1
+  end
+  string2.each_char do |char|
+    if hash[char] > 0
+      hash[char] -= 1
+    else
+      return false
+    end
+  end
+  hash.each_value do |value|
+    if value > 0
+      return false
+    end
+  end
+  return true
 end
