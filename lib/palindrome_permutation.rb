cee848d410f408b73e612567da1f072956e66e6f
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-08 22:51:24 -0800

    refactored palindrom_permutation with a hash table instead of just a hash, but could thing of ways to slim down the size of the internal array

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,22 +1,33 @@
 
 def palindrome_permutation?(string)
-  hash_table = {}
-  populate_hash_table(hash_table, string.chars)
-  hash_table.reject! { |key, value| value % 2 == 0 }
-  if hash_table.length > 1
+  return true if string.empty?
+
+  hash_table = hash_function(string.chars)
+  if hash_table.filter { |element| element.length.odd? }.length > 1
     return false
   else
     return true
   end
 end
 
-def populate_hash_table(hash_table, list)
-  list.each do |element|
-    if hash_table.include? element
-      hash_table[element] += 1
-    else
-      hash_table[element] = 1
-    end
+def hash_function(list)
+  hash_table = Array.new(26) { Array.new }
+
+  list.each do |char|
+    index = to_ascii(char)
+    hash_table[index] << char
   end
+
   return hash_table
+end
+
+
+def to_ascii(char)
+  if ("A".."Z").include? char
+    return char.ord - 65
+  elsif ("a".."z").include? char
+    return char.ord - 97
+  else
+    raise ArgumentError, "Invalid character encountered. Program exiting."
+  end
 end
