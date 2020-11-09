0418256d829d0a7c115741974d30742314653aee
Jasmine <jasylop@gmail.com>
2020-11-08 17:10:38 -0800

    palindrome permutation method

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,13 @@
 
 def palindrome_permutation?(string)
-
- 
+  string_chars = string.split("")
+  chars = Hash.new(0)
+  string_chars.each do |char|
+    if chars.has_key?(char)
+      chars.delete(char)
+    else
+      chars[char] += 1
+    end
+  end
+  return chars.length <= 1
 end # method
