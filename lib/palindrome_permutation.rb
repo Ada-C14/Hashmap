fcb4611a07473bc0e5defffb9bc0543086276e60
Stacy <lundquist.stacy@gmail.com>
2020-11-09 14:59:51 -0800

    Tried to decrease complexities

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,26 @@
-
 def palindrome_permutation?(string)
-
- 
-end # method
+  # hash = {}
+  #
+  # string.each_char do |letter|
+  #   hash[letter] = string.count(letter)
+  # end
+  #
+  # # print hash.values
+  #
+  # if string.length.even?
+  #   return hash.values.all? { |count| count % 2 == 0 }
+  # end
+  #
+  # if string.length.odd?
+  #
+  #   number_of_odd_values = 0
+  #
+  #   hash.values.each do |v|
+  #     if v.odd?
+  #       number_of_odd_values += 1
+  #     end
+  #   end
+  #
+  #   return false if number_of_odd_values.odd?
+  # end
+end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -15,5 +15,17 @@ def permutations?(string1, string2)
 
   hash == hash_2
 
+
+  # hash = Hash.new(0)
+  #
+  # string1.each_char do |letter|
+  #   hash[letter] += 1
+  # end
+  #
+  # string2.each_char do |letter_2|
+  #   hash[letter_2] -= 1
+  # end
+
+
 end
 
