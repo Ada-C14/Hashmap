00f11f5622077aa356351f37887bfe0ff87429f2
roshni-patel <rpatel01@wesleyan.edu>
2020-11-08 21:02:36 -0600

    Completed permutations method

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,20 @@
-
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  return true if string1.empty? && string2.empty?
+  return false if string1.length != string2.length
+
+  letters_count = {}
+  letters_count.default = 0
+
+  string1.each_char do |letter|
+    letters_count[letter] += 1
+  end
+
+  string2.each_char do |letter|
+    if letters_count[letter] > 0
+      letters_count[letter] -= 1
+    else
+      return false
+    end
+  end
+  return true
 end
