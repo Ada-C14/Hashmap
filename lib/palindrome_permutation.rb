708847267ec22d34971eed5714a6317c06ebad16
roshni-patel <rpatel01@wesleyan.edu>
2020-11-08 21:29:04 -0600

    Completed palindrome permutations method

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,19 @@
-
 def palindrome_permutation?(string)
+  return true if string.empty?
 
- 
-end # method
+  letters_count = {}
+  letters_count.default = 0
+
+  string.each_char do |letter|
+    letters_count[letter] += 1
+  end
+
+  odd = 0
+  string.each_char do |letter|
+    if letters_count[letter] % 2 != 0
+      odd += 1
+    end
+    return false if odd > 1
+  end
+  return true
+end
