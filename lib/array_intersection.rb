7cf5ff751d509bde159c1018f8f1f025da4ac618
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-09 21:32:20 -0800

    Finished array_intersection problem with tests passing

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,25 @@
-def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+#Design and implement a method that takes two integer arrays with unique values and returns their intersection in a new array.
+ def intersection(list1, list2)
+  return [] if list1.length == 0 || list2.length == 0
 
-end
+  hash_function = {}
+  result = []
+
+  list1.each do |int|
+   hash_function[int] = false
+  end
+
+  list2.each do |int|
+   if hash_function[int] == false
+    result << int
+   end
+  end
+  return result
+  end
+
+#Qstns:
+# 1. are lists the same length?
+
+#Pseudo Code
+# What happens if one list or both lists are nil?
+# Somehow, compare shortest list to longest list--helps with doing the least amount of comparisons--although trying to do this and it made the problemset much larger. Will not do for now.
