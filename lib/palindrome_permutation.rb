39c13e9d89de236c358865b160d0fa8acef59141
Schanen Ryan <schanen.r@gmail.com>
2020-11-09 22:05:18 -0800

    PALINDROME PERMUTATION: Method written and all tests passing.

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,16 +1,34 @@
 
 def palindrome_permutation?(string)
 
- 
-end # method
+  lookup_hash = {}
+  chars = string.split('')
+  counter = 0
 
+  return true if chars.length == 0
 
-def is_palindrome(s)
-  if s.length < 2
-    return true
-  elsif s[0] != s[-1]
-    return false
-  else
-    return is_palindrome(s[1..-2])
+  chars.each do |letter|
+    if lookup_hash[letter] == false || lookup_hash[letter] == nil
+      lookup_hash[letter] = letter
+    elsif lookup_hash[letter] == letter
+      lookup_hash[letter] = true
+    elsif lookup_hash
+      lookup_hash[letter] = letter
+      end
   end
+
+  chars.each do |letter|
+    if lookup_hash[letter] != true
+      counter += 1
+    end
+  end
+
+  if counter > 1
+    return false
+  end
+
+  return true
+
 end
+
+
