ffbc4ec2cce5e46f7ea01eebacb065b641f99ef8
Mackenzie <mvlofthus@gmail.com>
2020-11-09 20:30:51 -0800

    array intersection & permutations.  corrected names of permutations tests

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,38 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  #raise NotImplementedError, "permutations? not implemented"
+  hash = {}
+  first = string1.split("")
+  second = string2.split("")
+
+  if string1.length != string2.length
+    return false
+  elsif string1.length == 0
+    return true
+  end
+
+  first.each do |key|
+    if hash[key]
+      hash[key] += 1
+    else
+      hash[key] = 1
+    end
+  end
+
+  second.each do |letter|
+    if hash[letter]
+      hash[letter] -= 1
+    else
+      return false
+    end
+  end
+
+  hash.each do |key, value|
+    if hash[key] != 0
+      return false
+    end
+  end
+
+  return true
+
 end
