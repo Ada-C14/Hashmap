7485e95199e7550a9b7808455db032ee1309ce72
Blaine <b.atsbeha@gmail.com>
2020-11-10 09:37:26 -0800

    Trying to add . but something is going wrong.

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,16 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  # raise NotImplementedError, "Intersection not implemented"
+  hash = {}
+  array = []
 
+  list1.each do |x|
+    hash[x] = "fog"
+  end
+
+  list2.each do |y|
+    if hash[y]
+      array << y
+    end
+  end
+  return array
 end
