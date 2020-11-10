b8d06f95a0d96405f0476985a4f5c5b23c96e32a
KayKay-git <kayalew@yahoo.com>
2020-11-10 11:32:46 -0500

    Intersection and Palindrome Pemutation methods, tests; pass. Working on Permutations method

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,14 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  # raise NotImplementedError, "Intersection not implemented"
+  hash = {}
+  common = []
 
+  list1.each do |item|
+    hash[item] = true
+  end
+
+  list2.each do |item|
+    common << item if hash[item]
+  end
+  return common
 end
