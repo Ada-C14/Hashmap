469a53b90af6e233d25f3574d803c905484d4eec
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-11-10 08:27:08 -0800

    Palidrome permutation corrected + tests passing

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,12 +1,25 @@
-#time complexity: O(log n)
-#space complexity: O(log n)
+#space complexity: O(n), where n is length of string (space for hash)
+#time complexity: O(n), where n is length of string (time to build the hash)
 
 def palindrome_permutation(str)
-  string.downcase!
-  string.gsub!(/[^a-z0-9\s]/i, '')    #removes all non-alphanumeric chars
-  string.gsub!(/\s+/, '')    #removes spaces
+  str_hash = {}
 
-  (string.size/2).times.all? do |index|
-    string[index] == string[-index-1]  #returns true or false
+  str.each_char do |char|      #add every letter of string to a hash: {letter => occurrences}
+
+  if str_hash[char]
+      str_hash[char] += 1
+    else
+      str_hash[char] = 1
+    end
   end
+
+  odd_letters_counter = 0     #return false if more than 1 letter has an odd num of occurrences
+
+  str_hash.each_value do |value|
+    odd_letters_counter += 1 if value % 2 != 0
+    return false if odd_letters_counter > 1
+  end
+
+  return true
 end
+
