2e86ce82ea01622353c90fdb501b6fbe9fd3b850
Christina Minh <christina.minh@gmail.com>
2020-11-09 18:21:37 -0800

    Added array_intersection, permutations?, and palindrome_permutation methods

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,22 @@
+# Write a method which takes a string as an argument and returns true if the letters could be re-arranged into a palindrome.
+#
+# palindrome_permutation?("hello") => false
+# palindrome_permutation?("carrace") => true # because racecar is a palindrome
 
 def palindrome_permutation?(string)
+  lookup_hash = {}
 
- 
-end # method
+  length = string.length
+
+  length.times do |i|
+    letter = string[i]
+
+    if lookup_hash[letter]
+      lookup_hash.delete(letter)
+    else
+      lookup_hash[letter] = true
+    end
+  end
+
+  return lookup_hash.count <= 1
+end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,31 @@
-
+# Write a method which will take two strings as arguments and returns true if one string is a permutation of the other.
+#
+# permutations?("hello", "ehllo") => true
+# permutations?("pasta", "atsap") => true
+# permutations?("Pizza", "Pasta") => false
+# permutations?("", "") => true
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  return false if string1.length != string2.length
+
+  lookup_hash = Hash.new(0)
+
+  length = string1.length
+
+  length.times do |i|
+    letter = string1[i]
+
+    lookup_hash[letter] += 1
+  end
+
+  length.times do |i|
+    letter = string2[i]
+
+    if lookup_hash[letter]
+      lookup_hash[letter] -= 1
+    else
+      return false
+    end
+  end
+
+  return lookup_hash.values.all? { |value| value == 0}
 end
