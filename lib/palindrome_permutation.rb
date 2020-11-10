ec669edc8ed35e8476a6a57263ab35d695ba8c16
Hanh Solo <minhhanhnyc@gmail.com>
2020-11-09 21:23:13 -0800

    All methods completed

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,36 @@
 
-def palindrome_permutation?(string)
+# Time complexity: O(n)
+# Space complexity: O(n)
 
- 
-end # method
+def palindrome_permutation?(string)
+    hash = build_hash(string)
+    count_odd = 0
+    hash_value_array = hash.values
+    
+    hash_value_array.each do |value|
+        if value % 2 != 0
+            count_odd += 1
+        end
+    end
+
+    # A palindrome allows 1 or less single character
+    if count_odd > 1
+        return false
+    end
+    return true
+end 
+
+def build_hash(string)
+    hash = {}
+    string.each_char do |c|
+        if hash[c].nil?
+            hash[c] = 1
+        else
+            hash[c] += 1
+        end
+    end
+    return hash
+end
+
+string = "racecar"
+puts palindrome_permutation?(string)
