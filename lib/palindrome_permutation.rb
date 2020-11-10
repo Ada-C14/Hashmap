e5edf5a1b347a88130846b80f0ee372351affc69
Schanen Ryan <schanen.r@gmail.com>
2020-11-09 21:37:50 -0800

    INTERSECTION & PERMUTATION: all tests passing.

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -3,3 +3,14 @@ def palindrome_permutation?(string)
 
  
 end # method
+
+
+def is_palindrome(s)
+  if s.length < 2
+    return true
+  elsif s[0] != s[-1]
+    return false
+  else
+    return is_palindrome(s[1..-2])
+  end
+end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,36 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  # raise NotImplementedError, "permutations? not implemented"
+  lookup_hash = {}
+
+  chars1 = string1.split('')
+  chars2 = string2.split('')
+
+  return true if chars1.length == 0 && chars2.length == 0
+
+  chars1.each do |letter|
+    if lookup_hash[letter]
+      lookup_hash[letter] = 2
+    else
+      lookup_hash[letter] = true
+    end
+
+  end
+
+  chars2.each do |letter|
+    if lookup_hash[letter] == 2
+      lookup_hash[letter] = true
+    elsif lookup_hash[letter]
+      lookup_hash[letter] = false
+    else
+      return false
+    end
+  end
+
+  chars1.each do |letter|
+    return false if lookup_hash[letter] != false
+  end
+
+  return true
+
 end
