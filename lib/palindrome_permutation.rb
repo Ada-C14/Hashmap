d60b76f560bf486aaf538536bc3a0f75484c3388
Iris Lux <iris.lux0@gmail.com>
2020-11-09 21:03:14 -0800

    completed exercises

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,15 @@
-
 def palindrome_permutation?(string)
 
- 
-end # method
+  odd_num_chars = 0
+
+  letters_count = Hash.new(0)
+
+  string.each_char{|char| letters_count[char] += 1}
+
+  letters_count.each_value do |val|
+    odd_num_chars += 1 if val.odd?
+    return false if odd_num_chars > 1
+  end
+
+  return true
+end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,14 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+
+  letters_1 = Hash.new(0)
+  letters_2 = Hash.new(0)
+
+  string1.each_char{|char| letters_1[char] += 1}
+
+  string2.each_char{|char| letters_2[char] += 1}
+
+  letters_1.each_key{|letter| return false unless letters_1[letter] == letters_2[letter]}
+
+  return true
 end
