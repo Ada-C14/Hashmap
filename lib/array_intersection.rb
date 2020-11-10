d8bee1ef3782f320067fab45a24a07821a3530c7
Madeline <madelign@gmail.com>
2020-11-10 18:14:53 -0500

    partially completed array_intersection method, failing one test

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,26 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  def intersection(list1, list2)
+    intersection = []
 
+    if list1.length < list2.length
+      smaller_list = list1
+      larger_list = list2
+    else
+      larger_list = list1
+      smaller_list = list2
+    end
+
+    lookup = {}
+    smaller_list.each do |number|
+      lookup[number] = "true"
+    end
+
+    larger_list.each do |number|
+      if lookup.include? number
+        intersection << number
+      end
+    end
+
+    return intersection
+  end
 end
