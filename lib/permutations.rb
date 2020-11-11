9aa408eb01af1c4cd15616333f3d10e1e3e5a7b0
Madeline <madelign@gmail.com>
2020-11-11 01:31:54 -0500

    finished permutations method and passed tests

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,17 @@
-
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  lookup = {}
+  string1.chars do |letter|
+    lookup[letter] = false
+  end
+
+  string2.chars do |letter|
+    if lookup.include?(letter)
+      lookup[letter] = true
+    end
+  end
+
+  if lookup.values.all?
+  else
+    return false
+  end
 end
