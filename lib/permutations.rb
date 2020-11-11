810da6a23e1e43efa7049077133c1036a1090cce
Alice Boone <alicegbonel@gmail.com>
2020-11-10 20:33:36 -0800

    Solving hashmap

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,23 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  string2_hash = {}
+  string1_hash = {}
+  string2.each_char do |letter|
+    if string2_hash.key?(letter)
+      string2_hash[letter] += 1
+    else
+      string2_hash[letter] = 1
+    end
+  end
+  string1.each_char do |letter|
+    if string1_hash.key?(letter)
+      string1_hash[letter] += 1
+    else
+      string1_hash[letter] = 1
+    end
+  end
+  return string1_hash == string2_hash
 end
