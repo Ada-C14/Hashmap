e89eb56fd92d80af94041a6643d1437bf470e694
Pauline Chane <pauline.chane@gmail.com>
2020-11-13 17:42:02 -0800

    sorry about that -- just redid the algorithm and i think this should bypass and account for # of each char

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -3,7 +3,23 @@
 
 def palindrome_permutation?(string)
   return true if string.empty?
-  max = string.length.even? ? string.length/2 : (string.length/2) + 1
-  palindrome_check = Hash[*string.split('').collect{ |a| [a, 0] }.flatten]
-  return palindrome_check.length == max || palindrome_check.length == 1 # if there is only one character hashed
+  hash = {}
+  string.each_char do |char|
+    # track number of same char in string
+    if hash.has_key?(char)
+      hash[char] += 1
+    else
+      hash[char] = 1
+    end
+  end
+
+  single_odd = false
+  hash.each do |char, freq|
+    if freq % 2 == 1
+      return false if single_odd
+      single_odd = true
+    end
+  end
+
+  return true
 end # method
