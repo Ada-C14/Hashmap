2da4d0db3b6bab254c11e6d351e563176b463481
Rachael Gomez <gomezrc1220@gmail.com>
2020-11-15 21:50:34 -0800

    this is deeply late so I had to just turn in what I had.

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,30 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
 
-end
+  return [] if list1 == nil || list2 == nil || list1.length == 0 || list2.length == 0
+
+  hash = {}
+  if list1.length < list2.length
+    list1.each do |num|
+      hash[num] = true
+    end
+  else
+    list2.each do |num|
+      hash[num] = true
+    end
+  end
+
+  result = []
+  if list1.length < list2.length
+    list2.each do |num|
+      result << num if hash[num]
+    end
+  else
+    list1.each do |num|
+      result << num if hash[num]
+    end
+  end
+
+  return result
+  end
+
+
--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,22 @@
 
 def palindrome_permutation?(string)
-
- 
-end # method
+  if string == ""
+    return true
+  end
+  hash = {}
+  string = string.split
+  string.each do |letter|
+    hash[letter] = hash[letter].nil? ? 1 : hash[letter] + 1
+  end
+  count = 0
+  negative_count = -1
+  until count == string.length
+  if string[count] != hash[negative_count]
+    return false
+  else
+    count += 1
+    negative_count -= 1
+  end
+  return true
+  end
+  end
