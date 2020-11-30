eb699f7dbed952f572f837ceac031b222ca88c71
Marj E <marj@marjorieetienne.com>
2020-11-30 07:48:52 -0800

    added solution for permutaions

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,22 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented" 
+  return false if string1.length != string2.length
+
+  hash = {}
+
+  string1.length.times do |index|
+    if hash.keys.include? hash[string1[index]]
+      hash[string1[index]] += 1
+    else
+      hash[string1[index]] = 1
+    end
+  end
+
+  string2.length.times do |index|
+    hash[string2[index]] += 1 if hash.keys.include? string2[index]
+  end
+
+  return hash.values.all?{ |value| value.even? }
 end
+
+permutations?('pasta', 'atsap')
