048d33ca99dcbf6ffa52be2e96d55f98ddde44a1
Leah Scott-Zechlin <scottzec@gmail.com>
2020-12-03 19:09:43 -0800

    last problem palindrome passing tests

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -8,6 +8,29 @@
 def palindrome_permutation?(string)
   hash = {}
 
+  string_array = string.split("")
+
+  string_array.each do |char|
+    if hash[char]
+      hash[char] += 1
+    else
+      hash[char] = 1
+    end
+  end
+
+  occurrence_array = hash.values
+
+  occurrence_array.delete_if &:even?
+
+  if occurrence_array.length <= 1
+    return true
+  else
+    return false
+  end
+  
+end
+
+
   # Hash of keys (letters) and values (occurrence) pairs
   # Each value except one must be even so that the "word" can be balanced to be a palindrome
   # IF there is more than one odd number of occurrence for a character, false
@@ -18,5 +41,3 @@ def palindrome_permutation?(string)
   # or filter array that only odd numbers remain and check the length of that array
   # hash.values
 
- 
-end
