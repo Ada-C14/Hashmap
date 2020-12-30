0689935a06478840b48ce962d709ae429cba3040
Jessica Chan <jwinchan@gmail.com>
2020-12-29 16:34:36 -0800

    created method for array intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,19 @@
-def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+# time complexity: O(n)
+# space complexity: O(n)
 
+def intersection(list1, list2)
+  new_hash = {}
+  intersections = []
+
+  list1.each do |int|
+    new_hash[int] = 1
+  end
+
+  list2.each do |num|
+    if new_hash.has_key?(num)
+      intersections << num
+    end
+  end
+
+  return intersections
 end
