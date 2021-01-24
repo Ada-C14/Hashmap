149e2ad5c2af3bfa4c64e86d5eecae91040344de
Maha-ElMais <mahaelmais@gmail.com>
2021-01-24 12:07:43 -0800

    intersection, plaid-perm, permutations

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,5 +1,16 @@
 
 def palindrome_permutation?(string)
 
- 
-end # method
+  string_array = string.split("")
+
+  hash = Hash.new(0)
+  string_array.each do |char|
+    if hash.has_key?(char)
+      hash.delete(char)
+    else
+      hash[char] += 1
+    end
+  end
+  return hash.length <= 1
+
+end
